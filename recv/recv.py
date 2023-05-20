c4e02c9967e1288281fa0adde89736daf913bd6e
Anna Zhukova <28830446+bindreams@users.noreply.github.com>
2023-05-20 21:44:05 +0300

    Small refactoring

--- recv/recv.py
+++ recv/recv.py
@@ -1,18 +1,23 @@
-import cv2
+import base64
+import hashlib
 import json
 import sys
-import os
-import base64
-from qreader import QReader
 from io import BytesIO
-import hashlib
 
-precise_reader = QReader()
+import cv2
+from qreader import QReader
+
+
+class DecodeError(RuntimeError):
+    pass
+
+# Precise decoder ======================================================================================================
+_precise_reader = QReader()
 def precise_decode(image):
 
     # Attempt 1: convert image as-is using the precise converter
     prepared_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    decoded_tuple = precise_reader.detect_and_decode(prepared_image)
+    decoded_tuple = _precise_reader.detect_and_decode(prepared_image)
     if len(decoded_tuple) > 0 and decoded_tuple[0] is not None:
         return decoded_tuple[0]
 
@@ -21,7 +26,7 @@ def precise_decode(image):
     prepared_image = cv2.threshold(prepared_image, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
     prepared_image = cv2.cvtColor(prepared_image, cv2.COLOR_GRAY2RGB)
 
-    decoded_tuple = precise_reader.detect_and_decode(prepared_image)
+    decoded_tuple = _precise_reader.detect_and_decode(prepared_image)
     if len(decoded_tuple) > 0 and decoded_tuple[0] is not None:
         return decoded_tuple[0]
 
@@ -31,21 +36,23 @@ def precise_decode(image):
         prepared_image = cv2.threshold(prepared_image, threshold, 255, cv2.THRESH_BINARY)[1]
         prepared_image = cv2.cvtColor(prepared_image, cv2.COLOR_GRAY2RGB)
 
-        decoded_tuple = precise_reader.detect_and_decode(prepared_image)
+        decoded_tuple = _precise_reader.detect_and_decode(prepared_image)
         if len(decoded_tuple) > 0 and decoded_tuple[0] is not None:
             return decoded_tuple[0]
 
     return None
 
-fast_reader = cv2.QRCodeDetector()
+# Fast decoder =========================================================================================================
+_fast_reader = cv2.QRCodeDetector()
 def fast_decode(image):
-    packet, bbox, _ = fast_reader.detectAndDecode(image)
+    packet = _fast_reader.detectAndDecode(image)[0]
 
     if len(packet) == 0:
         return None
 
     return packet
 
+# Qr code stream =======================================================================================================
 class QrStream:
     def __init__(self, path):
         self.stream = cv2.VideoCapture(path)
@@ -72,20 +79,17 @@ class QrStream:
         if self.rewind_index == 0:
             ok, image = self.stream.read()
             if not ok:
-                raise RuntimeError("End of video file reached")
+                raise DecodeError("end of file was reached before payload was received")
 
             self.backup.append(image)
             self.index += 1
-            #print(f"\rFrame {self.index}/{len(self)}", end="")
             return image
 
-        # print(f"\n\n{len(self.backup)}; {self.relative_index}")
         if self.rewind_index >= len(self.backup):
-            raise RuntimeError("Failed to read stream")
+            raise DecodeError(f"failed to decode the stream at frame {self.index - self.rewind_index}")
 
         image = self.backup[-self.rewind_index-1]
         self.rewind_index += 1
-        #print(f"\rFrame {self.index - self.rewind_index}/{len(self)} (Rewind)", end="")
         return image
 
     def next(self):
@@ -116,13 +120,13 @@ def print_status(stream, next_index, payload=None, payload_size=None):
         print(f"\r[{status}] {frame_status}; {payload_status}; decoded {next_index-1} blocks", end="")
 
 
-def main():
-    stream = QrStream(sys.argv[1])
+def decode(inpath, outpath):
+    stream = QrStream(inpath)
 
     payload = BytesIO()
 
     # Find header
-    while(True):
+    while True:
         packet = stream.next()
         print_status(stream, None)
 
@@ -137,7 +141,7 @@ def main():
 
     # Find frames
     next_index = 0
-    while(True):
+    while True:
         print_status(stream, next_index, payload, size)
         packet = stream.next()
 
@@ -152,32 +156,35 @@ def main():
             continue
 
         if index > next_index:
-            #print(f"\nFailed to find block {next_index}: got {index}; rewinding...")
             stream.rewind()
             continue
 
         payload.write(block)
+        stream.confirm_ok()
+        next_index += 1
+
         if len(payload.getbuffer()) > size:
-            print("len > size")
-            break
-            #payload = payload[:size]
+            raise DecodeError("received payload of a bigger size than expected")
 
         if len(payload.getbuffer()) == size:
             break
-        #print(f"; Block {next_index}", end="")
-        stream.confirm_ok()
-        next_index += 1
     print()
 
-    data = base64.b85decode(payload.getbuffer()[:size])
+    data = base64.b85decode(payload.getbuffer())
 
     m = hashlib.sha256()
     m.update(data)
-    print(m.hexdigest())
-    print(sha)
+    if m.hexdigest() != sha:
+        raise DecodeError("SHA256 did not match")
 
-    with open(sys.argv[2], "wb") as f:
+    with open(outpath, "wb") as f:
         f.write(data)
+
+def main():
+    try:
+        decode(sys.argv[1], sys.argv[2])
+    except DecodeError as e:
+        print(f"\nFailed to decode: {e}")
 
 if __name__ == "__main__":
     sys.exit(main())

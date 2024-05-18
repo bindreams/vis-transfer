a8e640a22fc835553e42e92e1a0393499ad11f1f
Anna Zhukova <28830446+bindreams@users.noreply.github.com>
2024-05-19 02:43:43 +0300

    Enforce static builds on Windows

--- recv/scripts/cmake/bundled-vcpkg.cmake
+++ recv/scripts/cmake/bundled-vcpkg.cmake
@@ -20,20 +20,22 @@ if (NOT EXISTS "${BUNDLED_VCPKG_PATH}")
 	)
 endif()
 
-# if(WIN32)
-# 	# Windows is the only platform with dynamic linkage by default. Force static.
-# 	if (DEFINED ENV{VCPKG_TARGET_TRIPLET})
-# 		set(VCPKG_TARGET_TRIPLET "$ENV{VCPKG_TARGET_TRIPLET}")  # To print a message later
-# 		unset(ENV{VCPKG_TARGET_TRIPLET})
-# 	endif()
+if(WIN32)
+	# Windows is the only platform with dynamic linkage by default. Force static.
+	if (DEFINED ENV{VCPKG_TARGET_TRIPLET})
+		set(VCPKG_TARGET_TRIPLET "$ENV{VCPKG_TARGET_TRIPLET}")  # To print a message later
+		unset(ENV{VCPKG_TARGET_TRIPLET})
+	endif()
 
-# 	if (DEFINED VCPKG_TARGET_TRIPLET)
-# 		message(WARNING "Vcpkg target triplet has been redefined from \"${VCPKG_TARGET_TRIPLET}\" to \"x64-windows-static\"")
-# 	endif()
+	if (DEFINED VCPKG_TARGET_TRIPLET)
+		string(REGEX REPLACE "^(\\w+)-windows$" "\\1-windows-static" NEW_VCPKG_TARGET_TRIPLET "${VCPKG_TARGET_TRIPLET}")
+		message(WARNING "Vcpkg target triplet has been redefined from \"${VCPKG_TARGET_TRIPLET}\" to \"${NEW_VCPKG_TARGET_TRIPLET}\"")
+	else()
+		set(NEW_VCPKG_TARGET_TRIPLET "x64-windows-static")
+	endif()
 
-# 	unset(VCPKG_TARGET_TRIPLET)
-# 	unset(VCPKG_TARGET_TRIPLET CACHE)
-# 	set(VCPKG_TARGET_TRIPLET "x64-windows-static" CACHE STRING "")
-# endif()
+	unset(VCPKG_TARGET_TRIPLET)
+	set(VCPKG_TARGET_TRIPLET "${NEW_VCPKG_TARGET_TRIPLET}" CACHE STRING "" FORCE)
+endif()
 
 include(${BUNDLED_VCPKG_TOOLCHAIN})

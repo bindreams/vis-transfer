8485b9f562c21647065a5d4ab51da2806a6e0ce3
Anna Zhukova <28830446+bindreams@users.noreply.github.com>
2024-05-19 23:32:15 +0300

    Add a generate option to send

--- send/src/vis_transfer/__main__.py
+++ send/src/vis_transfer/__main__.py
@@ -1,11 +1,28 @@
 import sys
+import argparse
 
 from PySide6.QtWidgets import QApplication
 
 from .interface import SetupWindow
 
+def cli():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(title="subcommands", dest="subcommand")
+
+    generate_parser = subparsers.add_parser("generate")
+    generate_parser.add_argument("input", type=argparse.FileType("rb"))
+    generate_parser.add_argument("-o", "--output", required=True)
+
+    return parser
+
 
 def main():
+    args = cli().parse_args()
+    if args.subcommand == "generate":
+        from .testing import generate_video
+        generate_video(args.input, output_path=args.output)
+        return
+
     app = QApplication([])
     setup = SetupWindow()
 
